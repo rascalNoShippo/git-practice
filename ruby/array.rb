22536a8cd5a8bd6a411ae3810d026b468fa62a27
EC2 Default User <ec2-user@ip-172-31-34-53.ap-northeast-1.compute.internal>
2022-09-01 06:18:39 +0000

    [Finish] Ruby_4

---
+++ ruby/array.rb
@@ -0,0 +1,5 @@
+names=["Git","HTML","CSS"]
+p names[1]
+
+tall={"Taro"=>185,"Jiro"=>170,"Hanako"=>150}
+p tall["Taro"]
---
+++ ruby/weight.rb
@@ -0,0 +1,5 @@
+name = "AAA"
+weight = 50
+
+puts "#{name}さんの体重は#{weight}kgです"
+
